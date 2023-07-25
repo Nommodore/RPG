b4e8a7efe265bddd2a8a2a2e040249f970bad7e4
Nommodore <133529743+Nommodore@users.noreply.github.com>
2023-07-25 20:22:39 -0300

    atualização de texto vermelho

--- script.js
+++ script.js
@@ -6,20 +6,11 @@ const quote = [
   "“Nossas <span>ações</span> não precisam ser lembradas, <span>pois nunca serão esquecidas.</span>” </br> <p>Wynnstan Seraharte - 3° Brigada Real de Suporte",
   "“O Momento em que você <span>fecha os olhos no campo de batalha</span> é o momento em que você <span>nunca mais os abre.</span>” </br> <p>Maverick Phantomhive - Divisão de Reconhecimento",
   "“Quando a <span>justiça</span> se curva, ela <span>quebra.</span>” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
-  "“Os que são <span>loucos</span> de verdade para acreditar que podem <span>mudar o mundo</span>, são aqueles que de fato, <span>o mudam!</span>” </br> <p> Tengen - ???",
+  "“Os que são <span>loucos de verdade</span> para acreditar que podem <span>mudar o mundo</span>, são aqueles que de fato, <span>o mudam!</span>” </br> <p> Tengen - ???",
   "“Há o <span>certo</span> e o <span>errado</span>, todo o resto é mera <span>racionalização.</span>” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
-  "“Vivemos num <span>eterno</span> ciclo mortal, a crueldade gera <span>sofrimento</span> e o sofrimento leva a <span>crueldade.</span> Devemos <span>acabar</span> com isso.” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
+  "“Vivemos num <span>eterno ciclo mortal,</span> a crueldade gera <span>sofrimento</span> e o sofrimento leva a <span>crueldade.</span> Devemos <span>acabar</span> com isso.” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
   "“<span>Ordem</span> e <span>disciplina</span> andam lado a lado, aqueles que não os <span>mantêm</span>, permanecem na <span>idiotice.</span>” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
-  "“Eu sou o <span>osso</span> da minha espada. Aço é meu <span>corpo</span> e fogo é meu <span>sangue</span>. No entanto, essas mãos nunca devem segurar nada enquanto eu <span>oro</span>.” </br> <p>Kyre - ???"
-
-
-
-
-
-
-
-
-
+  "“Eu sou o <span>osso da minha espada.</span> <span>Aço é meu corpo</span> e <span>fogo é meu sangue</span>. No entanto, essas mãos nunca devem segurar nada enquanto eu oro.” </br> <p>Kyre - ???"
 
 ];
 
