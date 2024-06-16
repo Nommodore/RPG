6a725de42adb6f34ca601ccc15022e9a04447c8e
Nommodore <133529743+Nommodore@users.noreply.github.com>
2024-06-16 00:28:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // frases
 const quote = [
-  "“<span>Todo mundo</span> que busca poder, sem exceção, <span>busca a batalha</span>. Você luta <span>para se tornar mais poderoso</span>? Ou você quer mais poder para lutar?” </br> <p>Kenpachi - 5° Exército Real Adrynniano</p>",
-  "“<span id='textver'>Nossas ações</span> não precisam ser lembradas, pois <span id='textver'>nunca serão esquecidas.</span>” </br> <p>Wynnstan Seraharte - 3° Brigada Real de Suporte",
-  "“O Momento em que você fecha <span id='textver'>os olhos</span> no campo de batalha é o momento em que você <span id='textver'>nunca mais</span> os <span>abre.</span>” </br> <p>Maverick Phantomhive - Divisão de Reconhecimento",
-  "“Quando <span id='textver'>a justiça</span> se curva, ela <span id='textver'>quebra.</span>” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
-  "“<span id='textver'>Os que são loucos de verdade</span> para acreditar que <span id='textver'>podem mudar o mundo</span>, são aqueles que de fato, o mudam!” </br> <p> Tengen - ???",
+  "<span id='textver'>Não há</span> arrependimentos. Se alguém pode orgulhar-se da sua vida, não deve desejar <span id='textver'>outra oportunidade.</span>” </br> <p>Evangeline Mordred - 2° Exército de Choque “Last Apocrypha”</p>",
   "“Há <span id='textver'>o certo e o errado</span>. Todo o resto <span id='textver'>é mera racionalização.</span>” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
-  "“<span>O mal</span> só <span>lhe</span> <span>serve</span> contra aquele que te causou <span>uma vez.</span></span>” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
+  "<span id='textver'>“Quem quiser praticar sempre a bondade em tudo o que faz, está fadado a sofrer, entre tantos que não são bons.” </br> <p>[REMOVIDO] - [REMOVIDO]</span id='textver>'",
   "“<span id='textver'>Ordem e disciplina</span> andam lado a lado. Aqueles que não <span>os mantêm</span>, permanecem <span>na idiotice.</span>” </br> <p>Elizabeth Windsor - 3° Batalhão de Infantaria Móvel",
-  "“Eu sou o osso da minha espada. Aço é meu corpo e fogo é meu sangue. No entanto, <span id='textver'>essas mãos nunca devem segurar nada enquanto eu oro.</span>” </br> <p>Kyre - ???"
+  "“Todas as vidam importam. Independente se as vidas que estão em minhas mão são inimigas ou amigas.” </br> <p>Dr. Fritz Fitzgerald - Médico Voluntário Adrynniano"
 
 ];
 
+// 11 batalhão de reconhecimento móvel
 // escolher frase aleatória
 function randomquote() {
   const randomIndexquote = Math.floor(Math.random() * quote.length)
@@ -24,6 +21,8 @@ function changequote() {
   text.innerHTML = (randomquote(quote));
 
 }
+
+
 
 // ativar função
 changequote();
@@ -53,13 +52,11 @@ function deleteIntro() {
 
   const camp = document.getElementById('campanhas');
   
+  
   setTimeout(() => {
     
     camp.style.display = 'block';
     
   }, 10000);
   
-  
-
-
 }
